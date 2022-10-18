124344a18ae59b283c8422888bfae45d22e805bd
Sulaymahn <sulmuk28@gmail.com>
2022-10-18 15:32:42 +0100

    Add project files.

---
+++ OutlookMailer/OutlookMailer/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using OutlookMailer.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace OutlookMailer
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var m = new Mailer(userEmail.Text, userEmailPassword.Text);
+            await m.SendEmailAsync(userName.Text, receiverName.Text, receiverEmail.Text, messageSubject.Text, messageBody.Text);
+
+            await DisplayAlert("SENT", "EMAIL SENT", "OKAY");
+        }
+    }
+}
---
+++ OutlookMailer/OutlookMailer/Services/Mailer.cs
@@ -0,0 +1,41 @@
+﻿using MimeKit;
+using MailKit.Net.Smtp;
+using System.Threading.Tasks;
+
+namespace OutlookMailer.Services
+{
+    public class Mailer
+    {
+        public string UserEmail { get; private set; }
+        public string Password { get; private set; }
+
+        public Mailer(string userEmail, string password)
+        {
+            UserEmail = userEmail;
+            Password = password;
+        }
+        
+        public async Task SendEmailAsync(string sendername, string receivername, string email, string subject, string messagebody)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(sendername, UserEmail));
+            message.To.Add(new MailboxAddress(receivername, email));
+            message.Subject = subject;
+            message.Body = new TextPart("plain")
+            {
+                Text = messagebody
+            };
+
+            using (var client = new SmtpClient())
+            {   
+                client.Connect("smtp-mail.outlook.com", 587, false);
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+                client.Authenticate(UserEmail, Password);
+                await client.SendAsync(message);
+                client.Disconnect(true);
+            }
+
+            return;
+        }
+    }
+}
